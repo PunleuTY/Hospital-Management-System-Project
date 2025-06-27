84ded51b3662ce8c951ffa18b694fe2f3e4e85d3
Sitharath-s <sitharothwhy@gmail.com>
2025-06-27 21:20:35 +0700

    None

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowseRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Appointment from './components/Appointment.jsx';
@@ -7,6 +8,7 @@ import Doctor from './components/Doctor.jsx';
 import Patient from './components/Patient.jsx';
 import Login from './components/Login.jsx';
 import Staff from './components/Staff.jsx';
+import addAppointment from './components/Form/addAppointment.jsx';
 
 import Navigation from './layouts/Navigations.jsx';
 
@@ -15,12 +17,14 @@ export default function App(){
   return 
   <BrowseRouter>
     <Routes>
-      <Route path='/' element={}></Route>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
+      <Route path='/' element={<addAppointment />}></Route>
+      <Route path='/dashboard' element={<Dashboard />}></Route>
+      <Route path='/billing' element={<Billing />}></Route>
+      <Route path='/appointment' element={<Appointment />}></Route>
+      <Route path='/doctor' element={<Doctor />}></Route>
+      <Route path='/patient' element={<Patient />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/staff' element={<Staff />}></Route>
     </Routes>
   </BrowseRouter>
 
