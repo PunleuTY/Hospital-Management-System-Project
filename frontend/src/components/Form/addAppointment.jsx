b8cbbfe303134788bf75ac5aa7e3994dd43dfed4
Sitharath-s <sitharothwhy@gmail.com>
2025-06-28 21:00:22 +0700

    AddAppointment Done

--- frontend/src/components/Form/addAppointment.jsx
+++ frontend/src/components/Form/addAppointment.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react';
 import { Card, CardHeader, CardContent } from '../Common/Card.jsx';
 import Label from '../Common/Label.jsx';
 import Input from '../Common/Input.jsx';
+import Dropdown from '../Common/Dropdown.jsx';
+import Button from '../Common/Button.jsx';
+import { SiReacthookform } from "react-icons/si";
+
 /*
 import { Button } from '../Common/Button.jsx';
 import { Dropdown } from '../Common/Dropdown.jsx';
 import { Input } from '../Common/Input.jsx';
 */
-
-
 
 export default function AddAppointment() {
   //const navigate = useNavigate();
@@ -21,16 +23,21 @@ export default function AddAppointment() {
     purposeOfVisit: "",
     preferredDate: "",
     preferredTime: "",
-    preferredDoctor: "",
-    additionalNotes: "",
+    DoctorID: "",
+    PatientID:"",
   });
 
   function handlesubmit(e){
     e.preventDefault();
-    alert("Appointment scheduled successfully!");
+    alert("Appointment Added Successfully");
   }
 
-
+  const handleInputChange = (field, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }))
+  }
 
 
   return (
@@ -39,15 +46,15 @@ export default function AddAppointment() {
         <Card>
           {/*Header of the form*/}
           <CardHeader className="text-center">
-            <div className="flex items-center justify-between gab-3 mb-2">
-              {/*for icons <div></div>*/}
+            <div className="flex items-center justify-center gap-2">
+              <SiReacthookform className="text-2xl text-blue-1000" />
               <h1 className="text-xl font-semibold text-gray-900">Appointment Form</h1>
             </div>
           </CardHeader>
 
           {/*Content of the form*/}
           <CardContent>
-            <form on onSubmit={handlesubmit} className="space-y-6">
+            <form onSubmit={handlesubmit} className="space-y-6">
               {/*nothing important just keep border of Patient information part*/}
               <div>
                 <div className="space-y-4">
@@ -59,6 +66,57 @@ export default function AddAppointment() {
                       onChange={(e) => handleInputChange("purposeOfVisit", e.target.value)}
                     />
                   </div>
+                  
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                      <Label required>Date</Label>
+                      <Input
+                      type="date"
+                      value={formData.preferredDate}
+                      onChange={(e) => handleInputChange("preferredDate", e.target.value)} />
+                    </div>
+                    <div>
+                      <Label required>Time</Label>
+                      <Input
+                        placeholder="Appointment Time (24:00)"
+                        value={formData.preferredTime}
+                        onChange={(e) => handleInputChange("preferredTime", e.target.value)}
+                        
+                      />
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                      <Label required>Patient ID</Label>
+                      <Dropdown
+                      options={[1,2,3,4,5]}
+                      defaultLabel='Select Patient ID'
+                      onSelect={(value) => handleInputChange("PatientID", value)} />
+                    </div>
+                    <div> 
+                      <Label required>Doctor ID</Label>
+                      <Dropdown
+                        options={[1,2,3,4,5]}
+                        defaultLabel='Select Doctor ID'
+                        onSelect={(value) => handleInputChange("DoctorID",value)}/>
+                        
+                    </div>
+                  </div>
+
+                  <div className='flex flex-col gap-4'>
+                    <Button
+                      content={"Submit Form"}
+                      className="w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                      onClick={handlesubmit}
+                      // No need for + icon here  
+                      isAddIcon={false} 
+                    />
+                      
+                    
+                  </div>
+
+
 
                 </div>
 
