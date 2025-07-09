ab018f7b66e0631f9420ff99e6ef304cf6182076
Song Kimleangchhay <kimleangchhay.song.321@gmail.com>
2025-07-09 14:08:51 +0700

    update ui

--- frontend/src/components/addUser.jsx
+++ frontend/src/components/addUser.jsx
@@ -7,9 +7,10 @@ export default function AddUser() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [selectedRole, setSelectedRole] = useState("");
-
-  useEffect(() => {
-    console.log(selectedRole);
+  const [errors, setErrors] = useState({
+    userName: false,
+    password: false,
+    role: false,
   });
 
   // Role Option
@@ -23,13 +24,67 @@ export default function AddUser() {
     setUserName(e.target.value);
   };
 
+  const reset = () => {
+    setUserName("");
+    setPassword("");
+    setSelectedRole("");
+    setErrors({ userName: false, password: false, role: false });
+  };
+
+  const validate = () => {
+    let isValid = true;
+    const newErrors = { userName: false, password: false, role: false };
+
+    if (!userName) {
+      newErrors.userName = true;
+      isValid = false;
+    } else {
+      newErrors.userName = false;
+    }
+    if (!password) {
+      newErrors.password = true;
+      isValid = false;
+    } else {
+      newErrors.password = false;
+    }
+    if (!selectedRole) {
+      newErrors.role = true;
+      isValid = false;
+    } else {
+      newErrors.role = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = () => {
+    const validateStatus = validate();
+    if (validateStatus) {
+      const newUser = {
+        username: userName,
+        password: password,
+        role: selectedRole,
+      };
+      // Here you can add the logic to submit the form data
+      console.log("Form submitted successfully");
+      console.log(newUser);
+      reset();
+    }
+  };
+
   return (
     <div className="w-full p-6">
       <h1 className="text-3xl font-bold mb-1">Add User</h1>
       <div className="flex gap-5">
         <div className="flex-1 border border-(--color-gray) rounded-md p-3">
           <div>
-            <p>Username</p>
+            <div className="flex gap-2">
+              <p>Username</p>
+              {errors.userName && (
+                <p className="text-red-500">Please enter username</p>
+              )}
+            </div>
             <Input
               className="mb-3"
               type="text"
@@ -40,7 +95,12 @@ export default function AddUser() {
             />
           </div>
           <div className="flex-1">
-            <p>Password</p>
+            <div className="flex gap-2">
+              <p>Password</p>
+              {errors.password && (
+                <p className="text-red-500">Please enter password</p>
+              )}
+            </div>
             <Input
               className="mb-3"
               type="text"
@@ -51,14 +111,23 @@ export default function AddUser() {
             />
           </div>
           <div>
-            <p>User Role</p>
+            <div className="flex gap-2">
+              <p>Role</p>
+              {errors.role && (
+                <p className="text-red-500">Please select a role</p>
+              )}
+            </div>
             <div className="flex gap-3">
               <Dropdown
                 options={roleOptions}
                 defaultLabel="Choose a Role"
                 onSelect={setSelectedRole}
               />
-              <Button content={"Add User"} isAddIcon={true} />
+              <Button
+                content={"Add User"}
+                onClick={handleSubmit}
+                isAddIcon={true}
+              />
             </div>
           </div>
         </div>
